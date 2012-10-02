b50a06cfb8e9df6d8f42e7db774cea161340687a
Paul Phillips <paulp@improving.org>
2012-10-02 08:05:28 -0700

    All the actual changes of tpe to tpe_* or tpeHK.
    These are the call sites which formerly could be seen to
    call .tpe on a symbol with unapplied type parameters.  Now
    each such call site makes an explicit choice about what is
    intended for the result type.

--- src/compiler/scala/tools/nsc/backend/jvm/GenASM.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenASM.scala
@@ -83,7 +83,7 @@ abstract class GenASM extends SubComponent with BytecodeWriters {
         // Before erasure so we can identify generic mains.
         enteringErasure {
           val companion     = sym.linkedClassOfClass
-          val companionMain = companion.tpe.member(nme.main)
+          val companionMain = companion.tpe_*.member(nme.main)
 
           if (hasJavaMainMethod(companion))
             failNoForwarder("companion contains its own main method")
