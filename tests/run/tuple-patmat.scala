8f06603366bd9f559974fa11e623b6a74f02901f
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-01-24 10:48:49 +0100

    Typecheck tuple-patmat

--- tests/run/tuple-patmat.scala
+++ tests/run/tuple-patmat.scala
@@ -7,6 +7,10 @@ object Test {
 
     x1 match {
       case Box((a3, b3, A(a1, a2))) =>
+        a3: String
+        b3: Int
+        a1: Int
+        a2: Int
         assert(a3 == "a")
         assert(b3 == 2)
         assert(a1 == 3)
@@ -17,6 +21,8 @@ object Test {
 
     x2 match {
       case (a2, b2) =>
+        a2: String
+        b2: Int
         assert(a2 == "a")
         assert(b2 == 2)
     }
@@ -25,6 +31,9 @@ object Test {
 
     x3 match {
       case (a3, b3, c3) =>
+        a3: String
+        b3: Int
+        c3: String
         assert(a3 == "a")
         assert(b3 == 2)
         assert(c3 == "c")
@@ -34,6 +43,10 @@ object Test {
 
     x4 match {
       case (a4, b4, c4, d4) =>
+        a4: String
+        b4: Int
+        c4: String
+        d4: Int
         assert(a4 == "a")
         assert(b4 == 2)
         assert(c4 == "c")
@@ -44,6 +57,11 @@ object Test {
 
     x5 match {
       case (a5, b5, c5, d5, e5) =>
+        a5: String
+        b5: Int
+        c5: String
+        d5: Int
+        e5: String
         assert(a5 == "a")
         assert(b5 == 2)
         assert(c5 == "c")
@@ -54,13 +72,19 @@ object Test {
     val x6 = ("a", 2, "c", 4, "e", 6)
 
     x6 match {
-      case (a5, b5, c5, d5, e5, f5) =>
-        assert(a5 == "a")
-        assert(b5 == 2)
-        assert(c5 == "c")
-        assert(d5 == 4)
-        assert(e5 == "e")
-        assert(f5 == 6)
+      case (a6, b6, c6, d6, e6, f6) =>
+        a6: String
+        b6: Int
+        c6: String
+        d6: Int
+        e6: String
+        f6: Int
+        assert(a6 == "a")
+        assert(b6 == 2)
+        assert(c6 == "c")
+        assert(d6 == 4)
+        assert(e6 == "e")
+        assert(f6 == 6)
     }
   }
 
@@ -71,6 +95,6 @@ object Test {
     x match { case (a3, b3, c3) => () }
     x match { case (a4, b4, c4, d4) => () }
     x match { case (a5, b5, c5, d5, e5) => () }
-    x match { case (a5, b5, c5, d5, e5, f5) => () }
+    x match { case (a6, b6, c6, d6, e6, f6) => () }
   }
 }
