5e8aa20aa24edc34c8be3bacd7fbc8c4ce969295
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-02-06 13:59:36 +0100

    Revert "Extend ProductN for N <= 22, Product above that" (for now)
    This reverts commit c53bdae90ca8adbef9513e50281361df7e1eff16.
    
    Conflicts:
    	compiler/src/dotty/tools/dotc/ast/Desugar.scala

--- compiler/src/dotty/tools/dotc/ast/Desugar.scala
+++ compiler/src/dotty/tools/dotc/ast/Desugar.scala
@@ -333,6 +333,8 @@ object desugar {
     lazy val creatorExpr = New(classTypeRef, constrVparamss nestedMap refOfDef)
 
     // Methods to add to a case class C[..](p1: T1, ..., pN: Tn)(moreParams)
+    //     def productArity = N
+    //     def productElement(i: Int): Any = i match { ... }
     //     def _1 = this.p1
     //     ...
     //     def _N = this.pN
@@ -340,17 +342,18 @@ object desugar {
     //              pN: TN = pN: @uncheckedVariance)(moreParams) =
     //       new C[...](p1, ..., pN)(moreParams)
     //
-    // Above arity 22 we also synthesize:
-    //     def productArity = N
-    //     def productElement(i: Int): Any = i match { ... }
-    //
     // Note: copy default parameters need @uncheckedVariance; see
     // neg/t1843-variances.scala for a test case. The test would give
     // two errors without @uncheckedVariance, one of them spurious.
     val caseClassMeths = {
       def syntheticProperty(name: TermName, rhs: Tree) =
         DefDef(name, Nil, Nil, TypeTree(), rhs).withMods(synthetic)
-      def productArity = syntheticProperty(nme.productArity, Literal(Constant(arity)))
+      // The override here is less than ideal: user defined productArity / productElement
+      // methods would be silently ignored. This is necessary to compile `scala.TupleN`.
+      // The long term solution is to remove `ProductN` entirely from stdlib.
+      def productArity =
+        DefDef(nme.productArity, Nil, Nil, TypeTree(), Literal(Constant(arity)))
+          .withMods(Modifiers(Synthetic | Override))
       def productElement = {
         val param = makeSyntheticParameter(tpt = ref(defn.IntType))
         // case N => _${N + 1}
@@ -363,7 +366,7 @@ object desugar {
         val defaultCase = CaseDef(untpd.Ident(nme.WILDCARD), EmptyTree, error)
         val body = Match(refOfDef(param), (cases :+ defaultCase).toList)
         DefDef(nme.productElement, Nil, List(List(param)), TypeTree(defn.AnyType), body)
-          .withMods(synthetic)
+          .withMods(Modifiers(Synthetic | Override))
       }
       def productElemMeths = {
         val caseParams = constrVparamss.head.toArray
@@ -393,28 +396,19 @@ object desugar {
         }
       }
 
-      // Above MaxTupleArity we extend Product instead of ProductN, in this
-      // case we need to synthesise productElement & productArity.
-      def largeProductMeths =
-        if (arity > Definitions.MaxCaseClassTupleArity) List(productElement, productArity)
-        else Nil
-
       if (isCaseClass)
-        largeProductMeths ::: copyMeths ::: productElemMeths.toList
+        productElement :: productArity :: copyMeths ::: productElemMeths.toList
+      else if (isCaseObject)
+        productElement :: productArity :: Nil
       else Nil
     }
 
     def anyRef = ref(defn.AnyRefAlias.typeRef)
-    def productConstr(n: Int) = {
-      val tycon = scalaDot((tpnme.Product.toString + n).toTypeName)
-      val targs = constrVparamss.head map (_.tpt)
-      if (targs.isEmpty) tycon else AppliedTypeTree(tycon, targs)
-    }
 
-    // Case classes and case objects get a ProductN parent
-    var parents1 = parents
-    if (mods.is(Case))
-      parents1 = parents1 :+ productConstr(arity)
+    // Case classes and case objects get NameBasedPattern and Product parents
+    val parents1: List[Tree] =
+      if (mods.is(Case)) parents :+ scalaDot(nme.Product.toTypeName) :+ scalaDot(nme.NameBasedPattern.toTypeName)
+      else parents
 
     // The thicket which is the desugared version of the companion object
     //     synthetic object C extends parentTpt { defs }
