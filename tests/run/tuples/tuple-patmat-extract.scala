839ac8e48ee576b99268e174d0f7ff80133028f1
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-03-14 16:15:57 +0100

    wip co master

--- tests/run/tuples/tuple-patmat-extract.scala
+++ tests/run/tuples/tuple-patmat-extract.scala
@@ -1,11 +1,48 @@
+import dotty.TupleCons
+// sealed trait PP
+// final case class Pair[+A, +B <: PP](fst: A, snd: B) extends PP
+// final case object NIL                               extends PP
+
+trait TT
+trait TTCons[+H, +T <: TT] extends TT
+case object UNIT extends TT
+
+object TTCons {
+  def apply[H, T <: TT](h: H, t: T): TTCons[H, T] = ???
+  def unapply[H, T <: TT](t: TTCons[H, T]): Pair[H, T] = ???
+}
+
+class Pair[A, B](a: A, b: B) {
+  def isEmpty = false
+  def get     = this
+  def _1      = a
+  def _2      = b
+}
+
 object Test {
   def main(args: Array[String]): Unit = {
-    val Const, x = 0
-    val (Const, List(`x`, _, a), b) = (0, List(0, 1337, 1), 2)
-    val (_, (c, _)) = (2, (3, 4))
+    // val Const, x = 0
+    // val (Const, List(`x`, _, a), b) = (0, List(0, 1337, 1), 2)
+    // val (_, (c, _)) = (2, (3, 4))
 
-    assert(a == 1)
-    assert(b == 2)
-    assert(c == 3)
+    // assert(a == 1)
+    // assert(b == 2)
+    // assert(c == 3)
+
+    // Pair(Option(1), Pair("s", NIL)) match {
+    //   case Pair(Some(i), Pair(_, NIL)) => assert((i: Int) == 1)
+    // }
+
+    // TupleCons(Option(1), TupleCons("s", ())) match {
+    //   case TupleCons(Some(i), TupleCons(_, ())) => assert((i: Int) == 1)
+    // }
+
+    TTCons(Option(1), TTCons("s", UNIT)) match {
+      case TTCons(Some(i), TTCons(_, UNIT)) => assert((i: Int) == 1)
+    }
+
+    // (Option(1), "s") match {
+    //   case (Some(i), _) => assert((i: Int) == 1)
+    // }
   }
 }
--- tests/run/tuples/tuple-patmat-size.scala
+++ tests/run/tuples/tuple-patmat-size.scala
@@ -1,16 +1,16 @@
 object Test {
   def main(args: Array[String]) = {
     val x2 = ("a", 2)
-    // val x3 = ("a", 2, "c")
+    val x3 = ("a", 2, "c")
 
-    // (x2: Any) match {
-    //   case (a2, b2) =>
-    //     assert(a2 == "a")
-    //     assert(b2 == 2)
-    //   case (a3, b3, c3) =>
-    //     ???
-    //   case _ => ???
-    // }
+    (x2: Any) match {
+      case (a2, b2) =>
+        assert(a2 == "a")
+        assert(b2 == 2)
+      case (a3, b3, c3) =>
+        ???
+      case _ => ???
+    }
 
     (x2: Any) match {
       case (a3, b3, c3) =>
@@ -21,22 +21,22 @@ object Test {
       case _ => ???
     }
 
-    // (x3: Any) match {
-    //   case (a2, b2) =>
-    //     ???
-    //   case (a3, b3, c3) =>
-    //     assert(a3 == "a")
-    //     assert(b3 == 2)
-    //     assert(c3 == "c")
-    // }
+    (x3: Any) match {
+      case (a2, b2) =>
+        ???
+      case (a3, b3, c3) =>
+        assert(a3 == "a")
+        assert(b3 == 2)
+        assert(c3 == "c")
+    }
 
-    // (x3: Any) match {
-    //   case (a3, b3, c3) =>
-    //     assert(a3 == "a")
-    //     assert(b3 == 2)
-    //     assert(c3 == "c")
-    //   case (a2, b2) =>
-    //     ???
-    // }
+    (x3: Any) match {
+      case (a3, b3, c3) =>
+        assert(a3 == "a")
+        assert(b3 == 2)
+        assert(c3 == "c")
+      case (a2, b2) =>
+        ???
+    }
   }
 }
