5103527ec6d2c7a91e74b5944da206d952d1091d
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-06-29 17:59:53 +0200

    doit.sh

--- doit.sh
+++ doit.sh
@@ -2,16 +2,16 @@
 
 set -x
 
-for commit in 18b6cd70 aaecf81f 20e51125 622d519a 1e0c4a98 2ad686c9 7f7cad17 aeb1d0fd 9b1f2b5d 26c61740 098a42c8 1ce90917 fff06ef6 17611d46 13ff46e7 1058ae55 4dc26915 6841bc42 7c0d2668; do
+for commit in 18b6cd7 aaecf81 20e5112 622d519 1e0c4a9 2ad686c 7f7cad1 aeb1d0f 9b1f2b5 26c6174 098a42c 1ce9091 fff06ef 17611d4 13ff46e 1058ae5 4dc2691 6841bc4 7c0d266; do
 
   git checkout "$commit"
   msg=$(git log --format=%B -n 1)
 
-  sbt ";clean ;set bootstrapOptimised in ThisBuild := true; dotty-bootstrapped/publishLocal"
+  NIGHTLYBUILD=yes sbt ";clean ;set bootstrapOptimised in ThisBuild := true; dotty-bootstrapped/publishLocal"
 
   (
     cd /home/olivier/workspace/compiler-benchmark
-    sbt ";++0.2.0-bin-20170629-$commit-NIGHTLY cold -psource=vector -o cold-vector-$msg.log" > $msg.sbtlog
+    sbt ";++0.2.0-bin-20170629-$commit-NIGHTLY cold -psource=vector -o cold-vector-$msg.log"
   )
 
 done
