fff06ef6992965525091de84203cd514dfa83cca
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-06-29 17:46:15 +0200

    InlineLabelsCalledOnce

--- compiler/src/dotty/tools/dotc/transform/localopt/Simplify.scala
+++ compiler/src/dotty/tools/dotc/transform/localopt/Simplify.scala
@@ -41,8 +41,8 @@ class Simplify extends MiniPhaseTransform with IdentityDenotTransformer {
   private def beforeErasure: List[Optimisation] =
     // new InlineCaseIntrinsics(this)  ::
     // new RemoveUnnecessaryNullChecks ::
-    new InlineOptions               ::
-    // new InlineLabelsCalledOnce      ::
+    // new InlineOptions               ::
+    new InlineLabelsCalledOnce      ::
     // new Valify(this)                ::
     // new Devalify                    ::
     // new Jumpjump                    ::
